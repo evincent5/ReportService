2167f3864414f49a5b80e8d48d9349bcae3a46d7
evincent5 <free2nuel@gmail.com>
2017-06-30 07:43:52 -0500

    Add files via upload
    update

---
+++ ReportService/HelloService/HelloServiceHost/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.HelloClient host = new ServiceReference1.HelloClient();
+            host.Open();
+            Console.WriteLine("Host started @ " + DateTime.Now.ToString());
+
+            //var msg = proxy.GetData(101);
+            //Console.WriteLine(msg);
+            Console.ReadLine();
+        }
+    }
+}
