586ce6dc4b6fe033b0869219abf26dfdc389aa9d
evincent5 <free2nuel@gmail.com>
2017-06-30 07:41:28 -0500

    Add files via upload
    update

---
+++ ReportService/Host/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(ReportService.ReportService)))
+            {
+                host.Open();
+                Console.WriteLine("Host is opened @ " + DateTime.Now.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ ReportService/ReportService/IReportService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ReportService
+{
+    //Asso callback contract with service contract usindg CallbackContract attribute
+    [ServiceContract(CallbackContract = typeof(IReportServiceCallback))]
+    public interface IReportService
+    {
+        //since we have not set IsOneway=true, the operation is a Request/Reply operation
+        [OperationContract]
+        void ProcessReport();
+    }
+    //this is a callback contract
+    public interface IReportServiceCallback
+    {
+        //since we have not set IsOneway=true, the operation is a Request/Reply operation
+        [OperationContract]
+        void Progress(int percentageCompleted);
+    }
+}
